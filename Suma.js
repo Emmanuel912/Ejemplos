a9aaa644ccfdf655ffbcd2b111ed4fedade9c1e1
Emmanuel912 <111396297+Emmanuel912@users.noreply.github.com>
2022-11-22 18:45:13 -0600

    Add files via upload

---
+++ Suma.js
@@ -0,0 +1,19 @@
+var a=7;
+var b=4;
+suma = a+b;
+console.log("el resultado de la suma es:",suma);
+
+resta = a-b;
+console.log("el resultado de la resta:",resta);
+
+multiplicación = a*b;
+console.log("el resultado de la multiplicación es:",multiplicación);
+
+división = a/b;
+console.log("el resultado de la división es:",división);
+
+Cuadrado = a*a;
+console.log("el resultado de un numero multiplicado por si mismo 2 veces es:",Cuadrado);
+
+Cubo = a*a*a;
+console.log("el resultado de un numero multiplicado por si mismo 3 veces es:",Cubo);
